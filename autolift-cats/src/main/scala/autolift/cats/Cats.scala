1914ab4886b7e281f36e7999efeb2c7bb04860f2
frosforever <frosforever@users.noreply.github.com>
2016-01-02 14:33:50 -0500

    Add LiftExists

--- autolift-cats/src/main/scala/autolift/cats/Cats.scala
+++ autolift-cats/src/main/scala/autolift/cats/Cats.scala
@@ -4,6 +4,7 @@ import export._
 import autolift.cats._
 
 @reexports[CatsLiftMap, CatsLiftAp, CatsLiftFlatMap, CatsLiftFlatten, CatsLiftFilter,/* LiftJoin, LiftJoinWith, */
+  CatsLiftExists,
   CatsLiftA2, CatsLiftA3, CatsLiftA4, CatsLiftA5,
   CatsLiftM2, CatsLiftM3, CatsLiftM4, CatsLiftM5]
 object Cats extends Syntax with Context{
@@ -11,6 +12,7 @@ object Cats extends Syntax with Context{
   implicit def mkAp[Obj, Fn](implicit lift: CatsLiftAp[Obj, Fn]): CatsLiftAp.Aux[Obj, Fn, lift.Out] = lift
   implicit def mkFM[Obj, Fn](implicit lift: CatsLiftFlatMap[Obj, Fn]): CatsLiftFlatMap.Aux[Obj, Fn, lift.Out] = lift
   implicit def mkFl[M[_], Obj](implicit lift: CatsLiftFlatten[M, Obj]): CatsLiftFlatten.Aux[M, Obj, lift.Out] = lift
+  implicit def mkExists[Obj, Fn](implicit lift: CatsLiftExists[Obj, Fn]): CatsLiftExists.Aux[Obj, Fn, lift.Out] = lift
   //implicit def mkJ[Obj1, Obj2](implicit lift: LiftJoin[Obj1, Obj2]): LiftJoin.Aux[Obj1, Obj2, lift.Out] = lift
   //implicit def mkJw[Obj1, Obj2, Fn](implicit lift: LiftJoinWith[Obj1, Obj2, Fn]): LiftJoinWith.Aux[Obj1, Obj2, Fn, lift.Out] = lift
 }
